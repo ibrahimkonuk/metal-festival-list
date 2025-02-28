ab48d64de3525ce36147b61ac1b0511a56291049
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2025-02-28 13:48:01 +0100

    feat: integrate search functionality into MetalFestivalList

--- src/pages/MetalFestivalList.tsx
+++ src/pages/MetalFestivalList.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { useQueryFestivals } from "../api/generated/apiComponents";
 import { Card, Grid, LoadingOverlay, Text, Title, Alert } from "@mantine/core";
 import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 import { formatDateTime } from "./../utils/dateUtils";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { filterFestivals } from "../utils/searchUtils";
+import SearchBar from "../components/SearchBar";
 
 const MetalFestivalList = () => {
   const { data, isLoading, error } = useQueryFestivals({});
+  const [searchTerm, setSearchTerm] = useState("");
 
   if (isLoading) {
     return (
@@ -28,44 +32,53 @@ const MetalFestivalList = () => {
     );
   }
 
+  const filteredFestivals = filterFestivals(data || [], searchTerm);
+
   return (
     <div style={{ position: "relative", margin: 20 }}>
       <Title order={1} mb="md">
         Metal Festivals
       </Title>
 
-      <Grid>
-        {data?.map((festival) => (
-          <Grid.Col span={{ base: 12, md: 6, lg: 4 }} key={festival.id}>
-            <Card
-              shadow="sm"
-              padding="lg"
-              radius="md"
-              withBorder
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                height: "100%",
-              }}
-            >
-              <Title order={3}>
-                {festival.name || "Festival Name Not Available"}
-              </Title>
-              <Text mt="sm">
-                {festival.location?.city ? `${festival.location.city}/` : ""}
-                {festival.location?.country || "Location Not Available"}
-              </Text>
-              <Text>
-                Date: {festival.start ? formatDateTime(festival.start) : "N/A"}{" "}
-                - {festival.end ? formatDateTime(festival.end) : "N/A"}
-              </Text>
-              <Text lineClamp={3} mt="sm">
-                {festival.info}
-              </Text>
-            </Card>
-          </Grid.Col>
-        ))}
-      </Grid>
+      <SearchBar
+        placeholder="Search festivals by name, city, country, or info..."
+        onSearch={setSearchTerm}
+      />
+      {filteredFestivals && (
+        <Grid>
+          {filteredFestivals.map((festival) => (
+            <Grid.Col span={{ base: 12, md: 6, lg: 4 }} key={festival.id}>
+              <Card
+                shadow="sm"
+                padding="lg"
+                radius="md"
+                withBorder
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                }}
+              >
+                <Title order={3}>
+                  {festival.name || "Festival Name Not Available"}
+                </Title>
+                <Text mt="sm">
+                  {festival.location?.city ? `${festival.location.city}/` : ""}
+                  {festival.location?.country || "Location Not Available"}
+                </Text>
+                <Text>
+                  Date:{" "}
+                  {festival.start ? formatDateTime(festival.start) : "N/A"} -{" "}
+                  {festival.end ? formatDateTime(festival.end) : "N/A"}
+                </Text>
+                <Text lineClamp={3} mt="sm">
+                  {festival.info}
+                </Text>
+              </Card>
+            </Grid.Col>
+          ))}
+        </Grid>
+      )}
     </div>
   );
 };
